7a28d5ff056d6e7fc02cc0f8a2c53d166cf13ea7
Totba <theodesmoulins@gmail.com>
2023-12-07 15:50:53 +0100

    navbar + accueil utilisateur

---
+++ src/components/navbar.vue
@@ -0,0 +1,67 @@
+<!-- Navbar.vue -->
+
+<template>
+    <div class="navbar">
+        <div class="search-bar">
+            <input v-model="searchQuery" type="text" class="search-input" placeholder="Rechercher...">
+        </div>
+        <div class="user-logo">
+            <img src="../assets/userLogo.jpeg" alt="User Logo">
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({  
+    data() {
+        return {
+            searchQuery: '',
+        };
+    },
+});
+</script>
+
+<style scoped>
+
+.navbar {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    background-color: #333;
+    padding: 15px;
+    display: flex;
+    flex-direction: row-reverse;
+    align-items: center;
+    z-index: 1000; /* Assurez-vous que la navbar est au-dessus du contenu */
+}
+.search-bar {
+    flex-grow: 1;
+    padding: 0 40px;
+    width: 50%;
+}
+
+.search-input {
+    width: 100%;
+    padding: 8px;
+    border: none;
+    border-radius: 5px;
+    font-size: 16px;
+}
+
+.user-logo {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: #ddd;
+    overflow: hidden;
+}
+
+.user-logo img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+</style>
---
+++ src/views/AccueilView.vue
@@ -0,0 +1,107 @@
+<!-- BookPage.vue -->
+
+<template>
+  <Navbar/>
+  <div>
+    <h1>Liste des Livres</h1>
+    <div class="book-container">
+      <BookComponent
+        v-for="(book, index) in this.books"
+        :key="index"
+        :title="book.title"
+        :author="book.author"
+        :publicationDate="book.publicationDate"
+        :description="book.description"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BookComponent from '../components/livreComponents.vue';
+import Navbar from '../components/navbar.vue';
+
+export default defineComponent({
+  components: {
+    BookComponent,
+    Navbar
+  },
+  data() {
+    return {
+      books: [
+        {
+          title: 'Titre du Livre 1',
+          author: 'Auteur 1',
+          publicationDate: '2022-01-01',
+          description: 'Ceci est le début de la description du livre 1.',
+        },
+        {
+          title: 'Titre du Livre 2',
+          author: 'Auteur 2',
+          publicationDate: '2022-02-15',
+          description: 'Ceci est le début de la description du livre 2.',
+        },
+        {
+          title: 'Titre du Livre 3',
+          author: 'Auteur 3',
+          publicationDate: '2022-02-15',
+          description: 'Dans un monde où la technologie et la magie se rencontrent, suivez l épopée captivante d un jeune prodige, héritier d un pouvoir ancien. Entre alliances fragiles et trahisons inattendues, l aventure dévoile des secrets enfouis depuis des siècles, mettant en péril l équilibre fragile entre les royaumes.',
+        },
+        {
+          title: 'Titre du Livre 1',
+          author: 'Auteur 1',
+          publicationDate: '2022-01-01',
+          description: 'Ceci est le début de la description du livre 1.',
+        },
+        {
+          title: 'Titre du Livre 2',
+          author: 'Auteur 2',
+          publicationDate: '2022-02-15',
+          description: 'Ceci est le début de la description du livre 2.',
+        },
+        {
+          title: 'Titre du Livre 3',
+          author: 'Auteur 3',
+          publicationDate: '2022-02-15',
+          description: 'Dans un monde où la technologie et la magie se rencontrent, suivez l épopée captivante d un jeune prodige, héritier d un pouvoir ancien. Entre alliances fragiles et trahisons inattendues, l aventure dévoile des secrets enfouis depuis des siècles, mettant en péril l équilibre fragile entre les royaumes.',
+        },
+        {
+          title: 'Titre du Livre 1',
+          author: 'Auteur 1',
+          publicationDate: '2022-01-01',
+          description: 'Ceci est le début de la description du livre 1.',
+        },
+        {
+          title: 'Titre du Livre 2',
+          author: 'Auteur 2',
+          publicationDate: '2022-02-15',
+          description: 'Ceci est le début de la description du livre 2.',
+        },
+        {
+          title: 'Titre du Livre 3',
+          author: 'Auteur 3',
+          publicationDate: '2022-02-15',
+          description: 'Dans un monde où la technologie et la magie se rencontrent, suivez l épopée captivante d un jeune prodige, héritier d un pouvoir ancien. Entre alliances fragiles et trahisons inattendues, l aventure dévoile des secrets enfouis depuis des siècles, mettant en péril l équilibre fragile entre les royaumes.',
+        },
+        // Ajoutez d'autres livres selon vos besoins
+      ] as Book[], // Assurez-vous de spécifier le type Book pour le tableau books
+    };
+  },
+});
+  
+interface Book {
+  title: string;
+  author: string;
+  publicationDate: string;
+  description: string;
+}
+</script>
+
+<style scoped>
+.book-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+</style>
