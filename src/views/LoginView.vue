c1ebcd5e690b45080da20fd871f954d1a77f7c78
Alan <alan.jordy1999@gmail.com>
2023-12-07 12:57:43 +0100

    login and register done

---
+++ src/views/LoginView.vue
@@ -0,0 +1,28 @@
+<template>
+    
+    <form @submit="login">
+        <h1>Connexion</h1>  
+        <div>
+            <label for="email">Email:</label>
+            <input type="email" id="email" v-model="email" required>
+        </div>
+        <div>
+            <label for="password">Mot de passe:</label>
+            <input type="password" id="password" v-model="password" required>
+        </div>
+        <button type="submit">Se connecter</button>
+        <RouterLink to="/register">S'inscrire</RouterLink>
+    </form>
+</template>
+
+<script lang="ts" setup>
+import { defineProps,ref } from 'vue';
+
+const email = ref('');
+const password = ref('');
+const login = (event: Event) => {
+    event.preventDefault();
+    // Effectuer l'action de connexion ici
+    console.log('Connexion avec email:', email.value, 'et mot de passe:', password.value);
+};
+</script>
